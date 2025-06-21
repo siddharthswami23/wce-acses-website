ce0442a290b08b11d8812bc9e71a18cafafed8ae
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2025-06-21 19:08:24 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
 import Contact from './sections/Contact.jsx';
-import Clients from './sections/Clients.jsx';
 import Projects from './sections/Projects.jsx';
 import WorkExperience from './sections/Experience.jsx';
 
@@ -14,7 +13,6 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
-      <Clients />
       <WorkExperience />
       <Contact />
       <Footer />
